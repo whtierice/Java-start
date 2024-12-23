b31a0ffb1819e541d4b10fc3b889b4ef5748d00c
noeul <kee12355@gmail.com>
2024-12-23 17:32:20 +0900

    Add files via upload

---
+++ 1. variable/ex/VarEx3.java
@@ -0,0 +1,17 @@
+package variable.ex;
+
+// long 타입의 변수를 선언하고, 그 변수를 백억으로 초기화한 후 출력하느 프로그램을 작성하시오.
+// boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화한 후 출력하는 프로그램을 작성하세요.
+
+
+public class VarEx3 {
+    public static void main(String[] args) {
+
+        long a = 10000000000L;
+        System.out.println(a);
+
+        boolean b = true;
+        System.out.println(b);
+
+    }
+}
