72bfd4a21cb531afa39fce2631eb9a4aa1007d9e
noeul <kee12355@gmail.com>
2024-12-23 17:26:07 +0900

    Add files via upload

---
+++ variable/ex/VarEx2.java
@@ -0,0 +1,16 @@
+package variable.ex;
+
+// 변수 num1을 선언하고, 이에 10을 할당하세요.
+// 변수 num1을 선언하고, 이에 20을 할당하세요.
+// 두 변수의 합을 구하고, 그 결과를 새로운 변수 sum에 저장하세요.
+// sum 변수의 값을 출력하세요.
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+    }
+}
